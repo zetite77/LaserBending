598fd8170a2c06e3b29e642a844bbbb1b3669b4a
gorbo28@novasoft.co.kr <gorbo28@novasoft.co.kr>
2022-08-22 14:43:47 +0900

    설정 정보 전부 병합

--- Page/3HistoryInfo.xaml.cs
+++ Page/3HistoryInfo.xaml.cs
@@ -18,9 +18,23 @@ namespace LaserBendingMeasurementSystem.Page
     /// </summary>
     public partial class MenuHistoryInfo : UserControl
     {
+        List<history> historys = null;
         public MenuHistoryInfo()
         {
             InitializeComponent();
+            historys = new List<history>();
+            historys.Add(new history() { recpy = "John", pass = "합", xy = "-", date = "2022.08.22 13:00:00" });
+            historys.Add(new history() { recpy = "Emma", pass = "합", xy = "-", date = "2022.08.22 13:04:00" });
+            historys.Add(new history() { recpy = "Sophia", pass = "불", xy = "x:2,y:3", date = "2022.08.22 13:08:00" });
+            his.ItemsSource = historys;
+        }
+        public class history
+        {
+            public string recpy { get; set; }
+            public string pass { get; set; }
+            public string xy { get; set; }
+            public string date { get; set; }
         }
     }
+    
 }
